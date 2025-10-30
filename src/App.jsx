8fdbdc334753ee2df50c71ecb337c2566948e0c1
gaudotjeremy-sketch <gaudotjeremy@gmail.com>
2025-10-30 20:38:02 +0100

    Create App component for Moov'Authume application

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Moov'Authume</h1>
+      <p>Application React prête à être connectée à Google Sheets</p>
+    </div>
+  );
+}
